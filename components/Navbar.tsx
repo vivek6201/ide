231ae83f149961090543852861a6527e106a08d6
vivek6201 <gvicky4567888@gmail.com>
2024-10-09 17:05:14 +0530

    changed metadata and nav title

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -3,8 +3,8 @@ import { ModeToggle } from './ui/ModeToggler'
 
 export default function Navbar() {
   return (
-    <div className='h-16 dark:bg-neutral-900 bg-gray-200 px-10 flex justify-between items-center'>
-        <p className='text-xl font-bold'>LetsCode Compiler</p>
+    <div className='h-16 dark:bg-neutral-900 bg-gray-200 px-5 md:px-10 flex justify-between items-center'>
+        <p className='text-xl font-bold'>CodersHub Compiler</p>
         <ModeToggle/>
     </div>
   )
